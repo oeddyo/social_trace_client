6c9fb3273320b3e2304c58590a4bf18a75d86dd4
eddie.xie <oeddyo@gmail.com>
2014-02-13 10:48:57 -0500

    push

---
+++ content.js
@@ -0,0 +1,74 @@
+
+
+
+var baseURL = "http://127.0.0.1:5000/";
+
+localStorage.clear();
+    /*
+       getIdFromServer = function(){
+       var res = $.ajax({
+       type : "GET",
+       url : "http://127.0.0.1:5000/get_id_from_server",
+       success: function(result) {
+       console.log("get id successfully " + result)
+       },
+       async : false
+       }).responseText;
+       console.log("return res = " + res)
+       return res
+       }
+       */
+    /* get id */
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    };
+
+function guid() {
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
+
+getMachineID = function (){
+    chrome.storage.local.get('machine-id', function(item){
+        var storedMacId = item['machine-id'];
+        console.log("tmp id = "+storedMacId)
+        if(!storedMacId) {
+            console.log("before get " + storedMacId)
+        //storedMacId = getIdFromServer()
+        storedMacId = guid();
+    console.log("after get " + storedMacId)
+        chrome.storage.local.set({'machine-id':storedMacId});
+
+        }
+    console.log("id = " + storedMacId)
+        macId = storedMacId;
+    });
+}
+getMachineID();
+
+$(document).ready( function (){
+        //bind("load", function () {
+    console.log("removing ... ")
+    var numObjs = $(".metadata.social-context").length
+    $(".metadata.social-context").remove().ready( function () {
+        console.log("still exist? -> " + $(".metadata.social-context"))
+        for(var i = 0; i<numObjs; i++){
+            var res = $.ajax({
+                type : "GET",
+                url :"http://127.0.0.1:5000/get_condition?r="+i,
+                success: function(result) {
+                    //console.log(result)
+                },
+                async: false
+            }).responseText;
+            console.log("replacing ... "+i)
+            console.log("before insert " + $(".account-group.js-recommend-link.js-user-profile-link.user-thumb").eq(i) )
+            $(".account-group.js-recommend-link.js-user-profile-link.user-thumb").eq(i).after('<small class="metadata social-context">' + JSON.parse(res)['text'] + '</small>')
+        }
+        event.preventDefault()
+    }
+    )
+})
+
