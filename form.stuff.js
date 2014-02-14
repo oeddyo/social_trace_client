213905b4babc2e93b08c0b209735ca1bd73b37e0
eddie.xie <oeddyo@gmail.com>
2014-02-14 09:41:31 -0500

    push

---
+++ form.stuff.js
@@ -0,0 +1,7 @@
+$(document).ready(function() { 
+    // bind 'myForm' and provide a simple callback function 
+    $('#myForm').ajaxForm(function() { 
+        alert("Thank you for your comment!"); 
+    }); 
+}); 
+
